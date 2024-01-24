31ee09f720f441759d0407f5631938bade8afd93
Sébastien Journiac <sebastien.journiac@le-campus-numerique.fr>
2024-01-24 11:10:47 +0100

    v1.3 - Ajout de la structure des dossiers - Ajout du Front Controller

--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
-Bienvenue sur le blog
+<?php
+$action = filter_input(INPUT_GET,"action", FILTER_SANITIZE_URL);
+$Hobby=filter_input(INPUT_GET,'Hobby', FILTER_SANITIZE_URL);
+$CV=filter_input(INPUT_GET,'CV', FILTER_SANITIZE_URL);
+$contact=filter_input(INPUT_GET,'contact', FILTER_SANITIZE_URL);
+$success = false;
+$routes=array(
+    'Hobby.php'=> "Hobby",
+    'contact.php'=> "contact",
+    'CV.php'=> "CV");
+foreach ($routes as $key => $value){
+if ($action == $value){
+    include "./".$key;
+    $success=true;
+}
+};
+if(!$success) {   echo "404 PAS TROUVE, VA AILLEURS !";
+}
