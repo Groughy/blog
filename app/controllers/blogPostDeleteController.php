40c115b12a85c6a60b04fb74193dd9aae6706783
Sébastien Journiac <sebastien.journiac@le-campus-numerique.fr>
2024-01-31 11:03:58 +0100

    v6.3.1 - Corrections mineures dans les fichiers blogPostDeleteController.php et blogPostData.php

--- app/controllers/blogPostDeleteController.php
+++ app/controllers/blogPostDeleteController.php
@@ -2,6 +2,6 @@
 include '../app/persistances/blogPostData.php';
 global $pdo;
 global $posts_id;
-$update_post = blogPostDelete($pdo, $posts_id);
+$delete_post = blogPostDelete($pdo, $posts_id);
 header('Location: ?');
 exit;
--- app/persistances/blogPostData.php
+++ app/persistances/blogPostData.php
@@ -1,7 +1,7 @@
 <?php
 /********************************************
  * Fonction d'affichage de tous les articles
-*********************************************/
+ *********************************************/
 function lastPostData(PDO $pdo)
 {
     $statement = $pdo->query('SELECT *
@@ -44,7 +44,8 @@ Where users_id = Users.id AND posts_id=$posts_id");
 /************************************
  * Fonction de création d'un article
  ***********************************/
-function blogPostCreate (PDO $pdo, $post_name, $post_content):void{
+function blogPostCreate(PDO $pdo, $post_name, $post_content): void
+{
     $pdo->query("INSERT INTO Posts (title, users_id, content, dateFrom)
 VALUES ('$post_name', '2', '$post_content', CURDATE())
 ");
@@ -53,17 +54,20 @@ VALUES ('$post_name', '2', '$post_content', CURDATE())
 /***************************************
  * Fonction de modification d'un article
  ***************************************/
-function blogPostUpdate(PDO $pdo, $posts_id, $post_name, $post_content){
-$update = $pdo->query("UPDATE Posts
+function blogPostUpdate(PDO $pdo, $posts_id, $post_name, $post_content)
+{
+    $update = $pdo->query("UPDATE Posts
 SET title ='$post_name', content='$post_content'
 WHERE id='$posts_id'");
-return $update;
+    return $update;
 }
+
 /**************************************
  * Fonction de suppression d'un article
  **************************************/
-function blogPostDelete(PDO $pdo,$posts_id):void
-{$pdo->query("DELETE
+function blogPostDelete(PDO $pdo, $posts_id): void
+{
+    $pdo->query("DELETE
 FROM Posts
 WHERE id='$posts_id'");
 }
